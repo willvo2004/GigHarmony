9f59e217a9f98a9715f84c50338bed6840009e7a
willvo2004 <willvo2004@gmail.com>
2024-08-02 02:51:57 -0400

    moving profile check

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import { Header } from "@/components/Header";
 import { HeroContent } from "@/components/HeroContent";
-import { checkProfile } from "@/lib/data";
 
 export default async function Home() {
-  const profile = await checkProfile();
   return (
     <main className="h-screen">
       <div className="wrapper-header sticky top-0 py-6 px-24 w-full bg-slate-50 dark:bg-gray-950 border-b">
